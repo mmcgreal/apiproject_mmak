9f6a982e921a8c27a5937c5cd3cd59d092ab613f
mmcgreal <mmcgreal14@yahoo.com>
2015-11-16 00:34:08 -0500

    app.py work

--- app.py
+++ app.py
@@ -1,39 +1,43 @@
-import urllib2,json
+#tumblr.py
 from flask import Flask, render_template
-
-
+import urllib2, json
 
 app = Flask(__name__)
 
-@app.route("/t")
-@app.route("/t/<tag>")
-def t(tag="penguin"):
-    url = """
-    http://api.tumblr.com/v2/tagged?tag=%s&api_key=6qjbDDaQ4vUogvpFIZ2UoaHuo6ykn1vMpjRYOdYOPCQI6dBw4K
-    """
-    url = url%(tag)
-    request = urllib2.urlopen(url)
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+def read():
+    with open ("data.txt", "r") as tags:
+    data=tags.read().replace('\n', '') #should separate each entry into a new array entry
+
+
+def apiCall(n):
+    request = urllib2.urlopen(n)
     result = request.read()
-    r = json.loads(result)
-    photos=[]
+    return json.loads(result)
+
+@app.route("/t")   
+@app.route("/t/<tag>")
+def main(tag=data): 
+    key="6qjbDDaQ4vUogvpFIZ2UoaHuo6ykn1vMpjRYOdYOPCQI6dBw4K"
+    uri="https://api.tumblr.com/v2/tagged?tag=%s&api_key=%s"
+    url = uri%(tag,key)
+
+   
+    #r['response'][1]['photos'][0]['original_size']['url']
+    photos = []
     for item in r['response']:
         try:
-            newphoto = item['photos'][0]['original_size']['url']
-            photos.append(newphoto)
+            photos.append(item['photos'][0]['original_size']['url'])
         except:
             pass
-    return render_template("tagged.html",urls=photos)
 
-@app.route("/")
-def index():
-    return "hello"
-
-
+    return render_template("photos.html",urls=photos)
 
 if __name__ == "__main__":
    app.debug = True
+   app.secret_key = "secret"
    app.run(host="0.0.0.0", port=8000)
-
-
-
-
