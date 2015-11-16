dd99fef9ea53fba46887d3763da1732ae7b67bd9
mmcgreal <mmcgreal14@yahoo.com>
2015-11-16 05:06:46 -0500

    altered app.py

--- app.py
+++ app.py
@@ -18,15 +18,17 @@ def apiCall(n): #n=url
     result = request.read()
     return json.loads(result)
 
-@app.route("/t")   
-@app.route("/t/<tag>")
+#@app.route("/t")   
+#@app.route("/t/<tag>")
+@app.route("/",methods=["GET","POST"])
 def main(): #defaulted to tebow for now
+    #tumblr search
     tag="Tebow"
     if request.method == "POST":
         tag = request.form["player"]
     for space in [' ']:
         tag = tag.replace(space, "%20")
-    basic = """ """ + tag + """&results=1"""
+    basic = """https://www.tumblr.com/tagged/""" + tag
     tag = apiCall(basic)
 
     if tag["response"]["players"]:
@@ -39,7 +41,39 @@ def main(): #defaulted to tebow for now
         tag = tag.replace(space, "%20")
     print tag
 
-    key="6qjbDDaQ4vUogvpFIZ2UoaHuo6ykn1vMpjRYOdYOPCQI6dBw4K"
+    key="XMM91Jn3oeBBMejxa0JG1U7mEY88CnVqyjSisOhC9oE55ejJIn "
+    uri="https://api.tumblr.com/v2/tagged?tag=%s&api_key=%s"
+    url = uri%(tag,key)
+
+   
+    #r['response'][1]['photos'][0]['original_size']['url']
+    photos = []
+    for item in r['response']:
+        try:
+            photos.append(item['photos'][0]['original_size']['url'])
+        except:
+            pass
+
+    #flickr search
+    tag="Tebow"
+    if request.method == "POST":
+        tag = request.form["player"]
+    for space in [' ']:
+        tag = tag.replace(space, "%20")
+    basic = """https://www.tumblr.com/tagged/""" + tag
+    tag = apiCall(basic)
+
+    if tag["response"]["players"]:
+        tag = tag["response"]["players"][0]["name"]
+    else:
+    tag = "Tebow"
+    player = tag
+    
+    for space in [' ']:
+        tag = tag.replace(space, "%20")
+    print tag
+
+    key="XMM91Jn3oeBBMejxa0JG1U7mEY88CnVqyjSisOhC9oE55ejJIn "
     uri="https://api.tumblr.com/v2/tagged?tag=%s&api_key=%s"
     url = uri%(tag,key)
 
