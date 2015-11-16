1b7e4a3b8277d9a2967231819641eda254e4ffd3
mmcgreal <mmcgreal14@yahoo.com>
2015-11-16 12:42:34 -0500

    final

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import urllib2, json, signal, time, thread, multiprocessing
+from threading import Thread
+import urllib2
+import json
+import random
 
 app = Flask(__name__)
 
@@ -8,87 +12,46 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-def read():
-    with open ("data.txt", "r") as tags:
-        data=tags.read().replace('\n', '') #should separate each entry into a new array entry
+@app.route("/t")
+def t():
 
-
-def apiCall(n): #n=url
-    request = urllib2.urlopen(n)
-    result = request.read()
-    return json.loads(result)
-
-#@app.route("/t")   
-#@app.route("/t/<tag>")
-@app.route("/t",methods=["GET","POST"])
-def main(): #defaulted to tebow for now
-    #tumblr search
-    tag="Tebow"
-    if request.method == "POST":
-        tag = request.form["player"]
-    for space in [' ']:
-        tag = tag.replace(space, "%20")
-    basic = """https://www.tumblr.com/tagged/""" + tag
-    tag = apiCall(basic)
-
-    if tag["response"]["players"]:
-        tag = tag["response"]["players"][0]["name"]
-    else:
-        tag = "Tebow"
-    player = tag
-    
-    for space in [' ']:
-        tag = tag.replace(space, "%20")
-    print tag
-
-    key="XMM91Jn3oeBBMejxa0JG1U7mEY88CnVqyjSisOhC9oE55ejJIn "
+    key="6qjbDDaQ4vUogvpFIZ2UoaHuo6ykn1vMpjRYOdYOPCQI6dBw4K"
     uri="https://api.tumblr.com/v2/tagged?tag=%s&api_key=%s"
-    url = uri%(tag,key)
 
-   
-    #r['response'][1]['photos'][0]['original_size']['url']
-    photos1 = []
+    url1 = uri%("insect",key)
+    request = urllib2.urlopen(url1)
+    result = request.read()
+    r = json.loads(result)
+
+    url2 = uri%("animal",key)
+    request2 = urllib2.urlopen(url2)
+    result2 = request2.read()
+    r2 = json.loads(result2)
+
+    #r['response'][0]['photos'][0]['original_size']['url']
+
+    photos1=[]
     for item in r['response']:
         try:
-            photos1.append(item['photos'][0]['original_size']['url'])
+            newphoto1 = item['photos'][0]['original_size']['url']
+            photos1.append(newphoto1)
         except:
             pass
 
-    #flickr search
-    tag="Tebow"
-    if request.method == "POST":
-        tag = request.form["player"]
-    for space in [' ']:
-        tag = tag.replace(space, "%20")
-    basic = """https://www.tumblr.com/tagged/""" + tag
-    tag = apiCall(basic)
-
-    if tag["response"]["players"]:
-        tag = tag["response"]["players"][0]["name"]
-    else:
-        tag = "Tebow"
-    player = tag
-    
-    for space in [' ']:
-        tag = tag.replace(space, "%20")
-    print tag
-
-    key="9d9c84df9d7a536068063892e42543c6"
-    uri="http://api.flickr.com/services/rest/&method=flickr.activity.userPhotos&api_key=%s"
-    url = uri%(key)
-
-   
-    #r['response'][1]['photos'][0]['original_size']['url']
-    photos2 = []
-    for item in r['response']:
+    photos2=[]
+    for item in r2['response']:
         try:
-            photos2.append(item['photos'][0]['original_size']['url'])
+            newphoto2 = item['photos'][0]['original_size']['url']
+            photos2.append(newphoto2)
         except:
             pass
 
-    return render_template("photos.html",urls1=photos1,urls2=photos2)
+    return render_template("tagged.html",urls1=photos1,urls2=photos2)
+
+
 
 if __name__ == "__main__":
    app.debug = True
    app.secret_key = "secret"
-   app.run(host="0.0.0.0", port=8000)
+   app.run(host="0.0.0.0", port=8000) 
+
